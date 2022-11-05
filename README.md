a05cecfcb52d785feff6f82ab42e16ab1fc86ef2
crissnb <crissnb472@gmail.com>
2022-11-05 10:45:23 +0100

    made some changes

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # SmallerPDF
-Make your PDF files great again! A simple PDF resize tool written in Rust.
+Make your PDF files great again! A super simple PDF resize tool using GhostScript.
 
 # Intention
 The main purpose of this program is to reduce the resolution of the PDF, so that you can easily share your large size PDF around online (e.g. Discord has file limit size of 8MB).
 This program is especially helpful if you want to reduce the file size of a PDF that contains handwritten notes (e.g. produced with apps like GoodNotes)
 
+# Installation
+
 # Usage
+smallerpdf
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
+use std::path::Path;
+use std::process::Command;
 use std::{env, process::exit};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() == 0 {
+    if args.len() < 2 {
         println!("Provide a file path!");
         exit(0);
     }
 
-    // Get file path from the first argument
-    let file_path = &args[1];
-    println!("Hello, world! {}", file_path);
+    // Check if file exists and is pdf
+    let file_path = Path::new(&args[1]);
+    if !file_path.exists() {
+        println!("File does not exist!");
+        exit(0);
+    }
+
+    // TODO: Ability to optimize an entire folder of documents, recursively
+    if file_path.extension().unwrap() != "pdf" {
+        println!("Provided file is not a PDF document!");
+        exit(0);
+    }
+
+    let mut gs_command = Command::new("gs");
+    gs_command
+        .arg("-dBATCH")
+        .arg("-dNOPAUSE")
+        .arg("-q")
+        .arg("-dCompatibilityLevel=1.4")
+        .arg("-dPDFSETTINGS=/screen")
+        .arg("-r72")
+        .arg("-sDEVICE=pdfwrite")
+        .arg("-sOutputFile=output.pdf")
+        .arg(&file_path);
+
+    Command::output(&mut gs_command).expect("Failure");
+
+    println!("Result saved in output.pdf!");
 }
